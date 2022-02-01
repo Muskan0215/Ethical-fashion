fb4c5124d4101e396cf2b34060ce1a2338f66891
Muskan0215 <agarwalmuskan734@gmail.com>
2022-02-01 16:57:18 +0530

    Made some changes in the animation

--- script.js
+++ script.js
@@ -6,9 +6,17 @@ $(document).ready(function(){
   });
 });
 
-
 $(document).ready(function(){
-  $('#searchbutton').click(function(){
-      $('#searchplace').toggle("slideleft");
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 2000, function(){
+        window.location.hash = hash;
+      });
+    } 
   });
 });
+
