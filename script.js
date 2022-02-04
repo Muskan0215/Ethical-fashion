88362ac078010a8540b7215a2a3d574c4baf834b
Muskan0215 <agarwalmuskan734@gmail.com>
2022-02-04 17:30:19 +0530

    Changes as per discussion

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 $(document).ready(function(){
   $('#collapse-navbar').click(function(){
-      $('.collapse').toggle("slide");
-      $('.content').toggle();
-      $('.arrow').toggle();
+      $('.collapse').show(function(){
+        $('.collapse').css({display:"flex"});
+      });
   });
 });
 
 $(document).ready(function(){
-  $("a").on('click', function(event) {
+  $('.close-button').click(function(){
+      $('.collapse').hide("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".body1 .collapse li a").on('click', function(event) {
     if (this.hash !== "") {
       event.preventDefault();
       var hash = this.hash;
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 2000, function(){
+      }, 1000, function(){
         window.location.hash = hash;
       });
     } 
@@ -21,8 +27,17 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-  $('#searchbutton').click(function(){
-      $('#searchplace').toggle("slideleft");
+  $(".arrow-div p a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 500, function(){
+        window.location.hash = hash;
+      });
+    } 
   });
 });
+
 
